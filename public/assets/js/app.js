f0d0e7ed9ed066cc3b841b35c502f3e70715cbec
Jidess <102791310+Jidess@users.noreply.github.com>
2022-06-15 11:29:47 +0200

    first commit

---
+++ public/assets/js/app.js
@@ -0,0 +1,11 @@
+var gateTab = document.getElementById("gate");
+var devTab = document.getElementById("mainDev");
+var devBtn = document.getElementById("devBtn")
+function openDevPage() {
+    
+    gateTab.style.display = "none";
+    
+    devTab.style.display = "flex";
+}
+
+devBtn.addEventListener("click", () => { openDevPage();})
