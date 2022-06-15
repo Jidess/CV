520f8738a314aabcd2f8807a285671dae024d7e5
Jidess <Mehdi.Benkebouche@gmail.com>
2022-06-16 01:56:23 +0200

    début des animations

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,12 +1,14 @@
 var gateTab = document.getElementById("gate");
 var devTab = document.getElementById("mainDev");
-var devBtn = document.getElementById("devBtn")
+var devBtn = document.getElementById("devBtn");
+var Profile = document.getElementById("pic")
 
 function openDevPage() {
     
     gateTab.style.display = "none";
     
     devTab.style.display = "flex";
+    Profile.classList.toggle("flicker")
 }
 
 devBtn.addEventListener("click", () => { openDevPage();})
