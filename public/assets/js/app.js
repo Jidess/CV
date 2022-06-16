ec4df849f42cf508ff63884dda75f6e76fa851d9
Jidess <102791310+Jidess@users.noreply.github.com>
2022-06-16 13:53:08 +0200

    animation des cards

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,14 +2,36 @@ var gateTab = document.getElementById("gate");
 var devTab = document.getElementById("mainDev");
 var devBtn = document.getElementById("devBtn");
 var Profile = document.getElementById("pic")
+// Create the observer
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        const square = entry.target.querySelectorAll('.card');
+
+        if (entry.isIntersecting) {
+        square.forEach(element => {
+            
+        
+            element.classList.add('flicker');
+        });
+            return; // if we added the class, exit the function
+        }
+
+        // We're not intersecting, so remove the class!
+        // square.classList.remove('flicker');
+    });
+});
+
 
 function openDevPage() {
-    
+
     gateTab.style.display = "none";
-    
+
     devTab.style.display = "flex";
     Profile.classList.toggle("flicker")
 }
 
-devBtn.addEventListener("click", () => { openDevPage();})
-
+devBtn.addEventListener("click", () => {
+    openDevPage();
+})
+// Tell the observer which elements to track
+observer.observe(document.querySelector('#portfolioCard'));
